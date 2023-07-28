b82638f247c029e7792a0e69c5856173108555c6
axolotle <nchesnais@mailoo.org>
2023-07-28 17:30:56 +0200

    use localeStorage to store isLoggedIn value

--- composables/states.ts
+++ composables/states.ts
@@ -1,3 +1,17 @@
-export const useIsLoggedIn = () => useState<boolean>('isLoggedIn', () => false)
+export const useIsLoggedIn = () => {
+  const isLoggedIn = useState<boolean>(
+    'isLoggedIn',
+    () => localStorage.getItem('isLoggedIn') === 'true',
+  )
+
+  return computed({
+    get: () => isLoggedIn.value,
+    set: (value) => {
+      isLoggedIn.value = value
+      localStorage.setItem('isLoggedIn', value.toString())
+    },
+  })
+}
+
 export const useRedirectUrl = () =>
   useState<string | null>('redirectUrl', () => null)
