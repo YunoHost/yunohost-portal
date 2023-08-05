c20987659c0cb1f3c60731c4548502995b1fbea7
axolotle <nchesnais@mailoo.org>
2023-08-05 16:06:28 +0200

    add basic error page

---
+++ error.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { H3Error } from 'h3'
+
+const head = useLocaleHead({
+  addDirAttribute: true,
+  addSeoAttributes: true,
+})
+
+const props = defineProps<{
+  error: H3Error
+}>()
+</script>
+
+<template>
+  <main
+    class="container mx-auto p-10 min-h-screen flex flex-col justify-center items-center"
+  >
+    <Html :lang="head.htmlAttrs?.lang" :dir="head.htmlAttrs?.dir"></Html>
+    <img class="logo" src="/assets/img/logo-white.svg" />
+    <div class="text-center mt-10">
+      <h1 class="text-7xl font-bold">{{ error.statusCode }}</h1>
+      <p class="py-6">{{ error.statusMessage }}</p>
+    </div>
+  </main>
+</template>
+
+<style>
+.logo {
+  width: 50%;
+  max-width: 200px;
+}
+</style>
