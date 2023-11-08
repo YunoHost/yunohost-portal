ec7e566708d59fcb1f7bee137572c27c8300de1c
axolotle <nchesnais@mailoo.org>
2023-11-08 19:05:47 +0100

    YIcon: add new icon component to deal with icon sizes

---
+++ components/YIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    name: string
+    size?: string
+  }>(),
+  {
+    size: '1.5em',
+  },
+)
+</script>
+
+<template>
+  <NuxtIcon :name="name" :style="`--size: ${size}`" />
+</template>
+
+<style scoped>
+.nuxt-icon :deep(svg) {
+  /* Do not use `v-bind(size)`, idk why bind is not working */
+  width: var(--size);
+  height: var(--size);
+  display: inline;
+}
+</style>
