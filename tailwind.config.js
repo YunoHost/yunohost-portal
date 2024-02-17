2d652abc2f71fcfe23775c188af9dc8be21ca8ed
axolotle <nchesnais@mailoo.org>
2024-02-17 03:59:19 +0100

    fix: add bg-color classes in tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   // Safelisting some classes to avoid content purge
   plugins: [require('daisyui')],
-  safelist: ['safelisted'],
+  safelist: [
+    'safelisted',
+    'bg-primary',
+    'bg-secondary',
+    'bg-accent',
+    'bg-neutral',
+    'bg-base-100',
+    'bg-base-200',
+    'bg-base-300',
+    'bg-base-content',
+    'bg-info',
+    'bg-success',
+    'bg-warning',
+    'bg-error',
+  ],
   daisyui: {
     themes: true,
   },
